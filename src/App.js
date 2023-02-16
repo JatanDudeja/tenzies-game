9ae2db1ab684cf890abf7fc123c0d9095ee2d136
Jatan Dudeja <dudeja.jatan04@gmail.com>
2023-02-16 22:51:15 +0530

    full project

--- src/App.js
+++ src/App.js
@@ -2,11 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Dice from './component/Dice'
 import {nanoid} from "nanoid"
+import Confetti from 'react-confetti'
 
 
 export default function App(){
 
     const [dice, setDice] = React.useState(allNewDice())
+    const [tenzies, setTenzies] = React.useState(false)
+
+    React.useEffect(function(){
+        let check = true
+        for(let i = 0; i < dice.length; i++){
+            if(dice[i].isHeld === false){
+                check = false
+                break;
+            }
+        }
+
+
+        if(check){
+            let check2 = true
+            let val = dice[0].value
+            for(let i = 1; i < dice.length; i++){
+                if(dice[i].value != val){
+                    check2 = false
+                    break;
+                }
+            }
+
+            if(check2){
+                setTenzies(true)
+                console.log("You Won!");
+            }
+        }
+
+
+
+    }, [dice])
     
     function generator(){
         return {
@@ -27,9 +59,15 @@ export default function App(){
 
 
     function rollDice(){
-        setDice(prevDice => prevDice.map(di => {
-            return di.isHeld ?  di : generator()
-        }))
+        if(!tenzies){
+            setDice(prevDice => prevDice.map(di => {
+                return di.isHeld ?  di : generator()
+            }))
+        }
+        else{
+            setTenzies(false)
+            setDice(allNewDice)
+        }
     }
 
     function holdDice(id){
@@ -52,10 +90,13 @@ export default function App(){
 
     return(
         <main className='app-container'>
+            {tenzies && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className='container'>
                 {diceNew}
             </div>
-            <button className = "roll-dice" onClick={rollDice}>Roll</button>
+            <button className = "roll-dice" onClick={rollDice}>{tenzies ? "Start Game" : "Roll"}</button>
         </main>
     )
 }
